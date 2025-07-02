60ff606d8a62ed6a16764bd84a44ee8854108744
SeieiFSetsuna <128948808+SeieiFSetsuna@users.noreply.github.com>
2025-07-02 16:06:23 +0800

    Update settings_page.dart

--- flutter/lib/mobile/pages/settings_page.dart
+++ flutter/lib/mobile/pages/settings_page.dart
@@ -862,7 +862,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
             SettingsTile(
               title: Text(translate("Privacy Statement")),
               onPressed: (context) =>
-                  launchUrlString('https://rustdesk.com/privacy.html'),
+                  launchUrlString('http://svlay.com/privacy'),
               leading: Icon(Icons.privacy_tip),
             )
           ],
