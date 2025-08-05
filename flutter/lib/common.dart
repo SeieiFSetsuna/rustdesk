628a9bdcd52d5edf031f2fc581bd69e2f0a209f8
SeieiFSetsuna <1291287000@qq.com>
2025-08-05 14:58:13 +0800

    Update common.dart

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3516,7 +3516,7 @@ Widget loadPowered(BuildContext context) {
     cursor: SystemMouseCursors.click,
     child: GestureDetector(
       onTap: () {
-        launchUrl(Uri.parse('https://rustdesk.com'));
+        launchUrl(Uri.parse('https://tianren.com'));
       },
       child: Opacity(
           opacity: 0.5,
